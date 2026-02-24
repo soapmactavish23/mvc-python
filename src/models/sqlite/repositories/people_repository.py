60611f0d659cac922b94f531b17646a73694841e
soapmactavish23 <ricknogueira1231@gmail.com>
2026-02-24 19:43:10 -0300

    Implementando People Repository Parte 1

---
+++ src/models/sqlite/repositories/people_repository.py
@@ -0,0 +1,20 @@
+from src.models.sqlite.entities.people import PeopleTable
+
+class PeopleRepository:
+    def __init__(self, db_connection):
+        self.__db_connection = db_connection
+
+    def insert_person(self, first_name: str, last_name: str, age: int, pet_id: int):
+        with self.__db_connection as database:
+            try:
+                person_data = PeopleTable(
+                    first_name=first_name,
+                    last_name=last_name,
+                    age=age,
+                    pet_id=pet_id
+                )
+                database.session.add(person_data)
+                database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,4 +1,7 @@
 import pytest
+from Tools.scripts.fixdiv import report
+
+from src.models.sqlite.repositories.people_repository import PeopleRepository
 from src.models.sqlite.repositories.pets_repository import PetsRepository
 from src.models.sqlite.settings.connection import db_connection_handler
 
@@ -17,3 +20,13 @@ def test_delete_pet():
 
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_insert_person():
+    first_name = "Test name"
+    last_name = "Test last name"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
