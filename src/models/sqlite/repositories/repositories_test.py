5b31f4d0bc27ffdbba9334cf1ef43050c25a12b1
soapmactavish23 <ricknogueira1231@gmail.com>
2026-02-24 20:03:29 -0300

    Implementando People Repository Parte 2

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,32 +1,41 @@
 import pytest
-from Tools.scripts.fixdiv import report
-
-from src.models.sqlite.repositories.people_repository import PeopleRepository
-from src.models.sqlite.repositories.pets_repository import PetsRepository
 from src.models.sqlite.settings.connection import db_connection_handler
+from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 db_connection_handler.connect_to_db()
 
-@pytest.mark.skip(reason="interacao com o banco de dados")
+
+@pytest.mark.skip(reason="interacao com o banco")
 def test_list_pets():
     repo = PetsRepository(db_connection_handler)
-    reponse = repo.list_pets()
-    print()
-    print(reponse)
+    response = repo.list_pets()
+    print(response)
 
-@pytest.mark.skip(reason="interacao com o banco de dados")
+
+@pytest.mark.skip(reason="interacao com o banco")
 def test_delete_pet():
     name = "belinha"
-
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
 
-@pytest.mark.skip(reason="interacao com o banco de dados")
+
+@pytest.mark.skip(reason="interacao com o banco")
 def test_insert_person():
-    first_name = "Test name"
-    last_name = "Test last name"
+    first_name = "test name"
+    last_name = "test last"
     age = 77
     pet_id = 2
 
     repo = PeopleRepository(db_connection_handler)
     repo.insert_person(first_name, last_name, age, pet_id)
+
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print(response)
+    print(response.pet_name)
