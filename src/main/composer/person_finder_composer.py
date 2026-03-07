42978ab958a027e4feecc743ac1a96a410ad970b
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-07 14:52:43 -0300

    Implementando demais Rotas Parte 2

---
+++ src/main/composer/person_finder_composer.py
@@ -0,0 +1,10 @@
+from src.controller.person_finder_controller import PersonFinderController
+from src.models.sqlite.repositories.people_repository import PeopleRepository
+from src.models.sqlite.settings.connection import db_connection_handler
+from src.views.person_finder_view import PersonFinderView
+
+
+def person_finder_composer():
+    model = PeopleRepository(db_connection_handler)
+    controller = PersonFinderController(model)
+    return PersonFinderView(controller)
