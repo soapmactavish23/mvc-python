10ea9d025c6c83042eb32ab6c9ed0fe57aad9b95
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-07 16:43:40 -0300

    Gerencia de Erros e Finalização

--- src/main/routes/pets_route.py
+++ src/main/routes/pets_route.py
@@ -1,5 +1,6 @@
 from flask import jsonify, Blueprint
 
+from src.errors.error_handler import handle_errors
 from src.main.composer.pet_deleter_composer import pet_deleter_composer
 from src.main.composer.pet_lister_composer import pet_lister_composer
 from src.views.http_types.http_request import HttpRequest
@@ -8,16 +9,24 @@ pet_route_bp = Blueprint('pet_routes', __name__)
 
 @pet_route_bp.route('/pets', methods=['GET'])
 def list_pets():
-    http_request = HttpRequest()
-    view = pet_lister_composer()
+    try:
+        http_request = HttpRequest()
+        view = pet_lister_composer()
 
-    http_response = view.handle(http_request)
-    return jsonify(http_response.body), http_response.status_code
+        http_response = view.handle(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @pet_route_bp.route("/pets/<name>", methods=['DELETE'])
 def delete_pets(name):
-    http_request = HttpRequest(param={"name": name})
-    view = pet_deleter_composer()
+    try:
+        http_request = HttpRequest(param={"name": name})
+        view = pet_deleter_composer()
 
-    http_response = view.handle(http_request)
-    return jsonify(http_response.body), http_response.status_code
+        http_response = view.handle(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
