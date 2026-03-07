9e9e97f76ab40e552f83c62f239a444d610cdad1
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-07 14:47:12 -0300

    Implementando Demais Rotas Parte 1

---
+++ src/main/composer/pet_deleter_composer.py
@@ -0,0 +1,9 @@
+from src.controller.pet_deleter_controller import PetDeleterController
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+from src.models.sqlite.settings.connection import db_connection_handler
+from src.views.pet_deleter_view import PetDeleterView
+
+def pet_deleter_composer():
+    model = PetsRepository(db_connection_handler)
+    controller = PetDeleterController(model)
+    return PetDeleterView(controller)
---
+++ src/main/composer/pet_lister_composer.py
@@ -0,0 +1,9 @@
+from src.controller.pet_lister_controller import PetListerController
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+from src.models.sqlite.settings.connection import db_connection_handler
+from src.views.pet_lister_view import PetListerView
+
+def pet_lister_composer():
+    model = PetsRepository(db_connection_handler)
+    controller = PetListerController(model)
+    return PetListerView(controller)
