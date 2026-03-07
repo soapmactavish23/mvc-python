cef2a53b5cea03e193e962affd158dc5928d1439
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-07 14:59:04 -0300

    Erros Personalizados

--- src/controller/person_creator_controller.py
+++ src/controller/person_creator_controller.py
@@ -2,6 +2,8 @@ import re
 from typing import Dict
 
 from src.controller.interfaces.person_creator_controller_interface import PersonCreatorControllerInterface
+from src.errors.error_types.http_bad_request import HttpBadRequestError
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
 
 
@@ -25,7 +27,7 @@ class PersonCreatorController(PersonCreatorControllerInterface):
         non_valid_caracteres = re.compile(r'[^a-zA-Z]')
 
         if non_valid_caracteres.search(first_name) or non_valid_caracteres.search(last_name):
-            raise Exception("Nome da pessoa invalido!")
+            raise HttpBadRequestError("Nome da pessoa invalido!")
 
     def __insert_person_in_db(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
         self.__people_repository.insert_person(first_name, last_name, age, pet_id)
